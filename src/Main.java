bc29bf31e62eb618fd67f063dcf5a0a4b2b3af10
oks404 <ksunya2004@list.ru>
2022-11-16 20:48:28 +1000

    Переделка 1, 2, 3 для задания 2

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-//  Задание 1
-        int age = 17;
+//  Задание 1.1
+        int age = 18;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
         }
-        if (age <18) {
+        else {
             System.out.println("Тебе нет 18, возвращайся позже.");
         }
-//  Задание 2
-        int ageHuman = 7;
+//  Задание 1.2
+        int ageHuman = 24;
         if (ageHuman >= 7 && ageHuman < 18) {
          System.out.println("Тебе уже можно пойти в школу.");
-        }
-        if (ageHuman >= 18 && ageHuman < 24) {
+        } else if (ageHuman >= 18 && ageHuman < 24) {
             System.out.println("Поздравляем! Вы закончили школу и можете пойти в университет.");
         }
-        if (ageHuman >= 24) {
+        else {
             System.out.println("Сочувствуем, но пора искать работу.");
         }
-//  Задание 3
-    int wagonCapacity = 102;
-    int seatPlaces = 60;
-    int standingPlaces = wagonCapacity - seatPlaces;
-    int ticketsSoldSeatPlaces = 60;
-    int ticketSoldStandingPlaces = 42;
-    int ticketSoldAll = ticketSoldStandingPlaces + ticketsSoldSeatPlaces;
-    if (ticketSoldAll < 102) {
+//  Задание 1.3
+        int wagonCapacity = 102;
+        int seatPlaces = 60;
+        int standingPlaces = wagonCapacity - seatPlaces;
+        int ticketsSoldSeatPlaces = 60;
+        int ticketSoldStandingPlaces = 42;
+        int ticketSoldAll = ticketSoldStandingPlaces + ticketsSoldSeatPlaces;
+        if (ticketSoldAll < 102) {
         System.out.println("Количество свободных мест: " + (wagonCapacity - ticketSoldAll) + ". Из них сидячих: " + (seatPlaces - ticketsSoldSeatPlaces) + ", стоячих: " + (standingPlaces - ticketSoldStandingPlaces));
-    }
-    else  {
+        }
+        else  {
         System.out.println("Простите, но свободных мест нет.");
-    }
+        }
+
+
 
     }
 }
